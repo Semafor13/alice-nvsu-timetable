838e3baea25a40d3d800e069ebf6fe98f8d9b0c8
semafor13 <Versatilefromnet@gmail.com>
2024-03-24 17:17:38 +0500

    Отредактировал функцию на случай, если пар в этот день нет

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 
 base_url = 'http://timetable.nvsu.ru/tm/index.php/timetable/show_timetable/'
 group_number = "8277"
-subgroup = "0"
+subgroup = "1"
 
 
 def get_current_date():
@@ -21,7 +21,7 @@ def get_tomorrow_date():
     return f"{day}_{month}_{year}"
 
 
-date = "22_04_2024"
+date = "02_05_2024"
 
 r = requests.get(f"{base_url}group/{group_number}//{subgroup}/?date={date}")
 soup = BeautifulSoup(r.text, "html.parser")
@@ -38,10 +38,19 @@ def clear_text(text: str) -> str:
     return cleared_text
 
 
-def get_day(tt: BeautifulSoup, d: int) -> list[BeautifulSoup]:
+def get_day(tt: BeautifulSoup, d: int) -> list[BeautifulSoup] | None:
     heads: list[BeautifulSoup] = tt.findAll("thead")
 
-    dates = [int(clear_text(h.find("div").get_text()).split(" ")[1]) for h in heads]
+    dates = []
+
+    for h in heads:
+
+        if h.find(class_="empty-day"):
+            continue
+
+        day_in_week = int(clear_text(h.find("div").get_text()).split(" ")[1])
+
+        dates.append(day_in_week)
 
     if d in dates:
         index_of_day_tr = dates.index(d)
@@ -53,31 +62,41 @@ def get_day(tt: BeautifulSoup, d: int) -> list[BeautifulSoup]:
 def get_data_from_tr(tr: BeautifulSoup):
     tds: list[BeautifulSoup] = tr.find_all("td")
 
-    pg_indexer = 0
+    indexer = 0
+
+    class_subgroup = None
 
     if len(tds) == 7:
-        pg_indexer = 1
+        indexer = 1
+
+        if tds[2].get_text():
+            class_subgroup = clear_text(tds[2].get_text())
 
     number_of_exercise = tds[1].find("small").get_text()
 
-    subject_data = clear_text(tds[2 + pg_indexer].get_text()).split("-")
+    subject_data = clear_text(tds[2 + indexer].get_text()).split("-")
 
     subject_type = subject_data[0].strip()
     subject_name = subject_data[1].strip()
 
-    teacher = clear_text(tds[3 + pg_indexer].find(class_="teacher").get_text()).split("(")[0].strip()
+    teacher = clear_text(tds[3 + indexer].find(class_="teacher").get_text()).split("(")[0].strip()
 
-    groups = clear_text(tds[4 + pg_indexer].find("div").get_text()).split(", ")
+    groups = clear_text(tds[4 + indexer].find("div").get_text()).split(", ")
 
-    auditory = clear_text(tds[5 + pg_indexer].find("a").get_text())
+    auditory = clear_text(tds[5 + indexer].find("a").get_text())
 
-    return number_of_exercise, subject_type, subject_name, teacher, groups, auditory
+    return number_of_exercise, class_subgroup, subject_type, subject_name, teacher, groups, auditory
 
 
-def get_all_classes(tt: BeautifulSoup, d: int) -> list:
+def get_all_classes(tt: BeautifulSoup, d: int) -> list | None:
+    day = get_day(tt, d)
+
+    if day is None:
+        return None
+
     classes = []
 
-    for tr in get_day(tt, d):
+    for tr in day:
         classes.append(get_data_from_tr(tr))
 
     return classes
